04add068497266a68d144c7d22531eba3d6c629b
gautamkirti8c@gmail.com <gautamkirti8c@gmail.com@github.com>
2019-10-04 16:05:49 +0530

    Copyright Modified

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="page-footer gradient-bg">
     <div class="footer-copyright">
         <div class="container">
-            © 2018 Copyright SagarMaheshwary
+            © 2019 Copyright GNDEC-EMS
         </div>
     </div>
 </footer>
